6ebb3f46f0afcb6643a680e59b2cf15843ef3058
matt venn <matt@mattvenn.net>
2022-11-03 19:14:20 +0100

    neaten up, remove logging, license

--- sssummarizer.py
+++ sssummarizer.py
@@ -2,27 +2,11 @@
 import glob
 import re
 import argparse
-import logging
 import os
-import sys
 import json
 
 
-# load all the json defs and combine into one big file
-def create_defs(args):
-    # replace this with a find
-    json_files = glob.glob('sky130_fd_sc_hd/latest/cells/*/definition.json')
-    definitions = {}
-    for json_file in json_files:
-        log.debug(json_file)
-        with open(json_file) as fh:
-            definition = json.load(fh)
-            definitions[definition['name']] = definition
-
-    with open('defs.json', 'w') as fh:
-        json.dump(definitions, fh)
-
-
+# Print the summaries
 def summarize(cell_count):
     with open('tags.json') as fh:
         tags = json.load(fh)
@@ -58,6 +42,7 @@ def summarize(cell_count):
                 pass
 
 
+# Parse the lib, cell and drive strength an OpenLane gate-level Verilog file
 def get_cell_count_from_gl(args):
     cell_count = {}
     total = 0
@@ -75,35 +60,34 @@ def get_cell_count_from_gl(args):
                     cell_count[cell_name] += 1
                 except KeyError:
                     cell_count[cell_name] = 1
-    logging.debug(f'total cells = {total}')
     return (cell_count)
 
 
+# Load all the json defs and combine into one big dict, keyed by cellname
+def create_defs(args):
+    # replace this with a find
+    json_files = glob.glob('sky130_fd_sc_hd/latest/cells/*/definition.json')
+    definitions = {}
+    for json_file in json_files:
+        with open(json_file) as fh:
+            definition = json.load(fh)
+            definitions[definition['name']] = definition
+
+    with open('defs.json', 'w') as fh:
+        json.dump(definitions, fh)
+
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Efabless project tool")
+    parser = argparse.ArgumentParser(description="Tiny Tapeout summary tool")
 
     parser.add_argument('--create-defs', help="create def file", action="store_const", const=True, default=False)
     parser.add_argument('--print-summary', help="print summary", action="store_const", const=True, default=False)
     parser.add_argument('--print-category', help="print category", action="store_const", const=True, default=False)
     parser.add_argument('--gl', help="gate level netlist")
-    parser.add_argument('--debug', help="debug logging", action="store_const", dest="loglevel", const=logging.DEBUG, default=logging.INFO)
     args = parser.parse_args()
 
-    # change directory to the script's path
+    # change directory to the script's path, to have access to json files
     os.chdir((os.path.dirname(os.path.realpath(__file__))))
-
-    # setup log
-    log_format = logging.Formatter('%(message)s')
-    # configure the client logging
-    log = logging.getLogger('')
-    # has to be set to debug as is the root logger
-    log.setLevel(args.loglevel)
-
-    # create console handler and set level to info
-    ch = logging.StreamHandler(sys.stdout)
-    # create formatter for console
-    ch.setFormatter(log_format)
-    log.addHandler(ch)
 
     # create map
     if args.create_defs:
