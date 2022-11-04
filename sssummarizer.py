178cf5b178aa9b7f882b3ac14fd464451bbcc2af
matt venn <matt@mattvenn.net>
2022-11-04 07:56:33 -0700

    sort by freq, add examples to the by category

--- sssummarizer.py
+++ sssummarizer.py
@@ -5,13 +5,12 @@ import argparse
 import os
 import json
 
-
+# can't find a google version
 CELL_URL = 'https://antmicro-skywater-pdk-docs.readthedocs.io/en/test-submodules-in-rtd/contents/libraries/sky130_fd_sc_ls/cells/'
 
 
 # Print the summaries
 def summarize(cell_count):
-
     script_dir = os.path.dirname(os.path.realpath(__file__))
 
     with open(os.path.join(script_dir, 'categories.json')) as fh:
@@ -19,37 +18,40 @@ def summarize(cell_count):
     with open(os.path.join(script_dir, 'defs.json')) as fh:
         defs = json.load(fh)
 
-    # print all used cells
+    # print all used cells, sorted by frequency
     total = 0
     if args.print_summary:
         print('# Cell usage')
         print()
         print('| Cell Name | Description | Count |')
         print('|-----------|-------------|-------|')
-        for cell_name in cell_count:
-            category = categories['map'][cell_name]
-            if cell_count[cell_name] > 0:
-                total += cell_count[cell_name]
-                cell_link = f'{CELL_URL}{cell_name}'
-                print(f'| [{cell_name}]({cell_link}) | {defs[cell_name]["description"]} |{cell_count[cell_name]} |')
+        for name, count in sorted(cell_count.items(), key=lambda item: item[1], reverse=True):
+            category = categories['map'][name]
+            if count > 0:
+                total += count
+                cell_link = f'{CELL_URL}{name}'
+                print(f'| [{name}]({cell_link}) | {defs[name]["description"]} |{count} |')
 
         print(f'| | Total | {total} |')
 
     if args.print_category:
         by_category = {}
         for cell_name in cell_count:
-            category = categories['map'][cell_name]
-            by_category[category] = cell_count[cell_name]
+            cat_index = categories['map'][cell_name]
+            cat_name = categories['categories'][cat_index]
+            if cat_name in by_category:
+                by_category[cat_name]['count'] += cell_count[cell_name]
+                by_category[cat_name]['examples'].append(cell_name)
+            else:
+                by_category[cat_name] = { 'count' : cell_count[cell_name], 'examples' : [cell_name] }
 
         print('# Cell usage by Category')
         print()
-        print('| Cell Category | Count |')
-        print('|---------------|-------|')
-        for index, cat_name in enumerate(categories['categories']):
-            try:
-                print(f'|{cat_name} | {by_category[index]}|')
-            except KeyError:
-                pass
+        print('| Category | Cells | Count |')
+        print('|---------------|----------|-------|')
+        for cat_name, cat_dict in sorted(by_category.items(), key=lambda x: x[1]['count'], reverse=True):
+            cell_links = [ f'[{name}]({CELL_URL}{name})' for name in cat_dict['examples'] ]
+            print(f'|{cat_name} | {" ".join(cell_links)} | {cat_dict["count"]}|')
 
 
 # Parse the lib, cell and drive strength an OpenLane gate-level Verilog file
